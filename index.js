118ef5f8a3090011dccb2482ad9506499efbbbbd
abuttle2 <alexbuttle@hotmail.co.uk>
2023-02-19 20:41:41 +0000

    Added test question using Inquirer

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//Test question
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+]);
